d7b1a290ec23078f853b1cb96888d52b0851c126
TomSievers <tom.sievers13@gmail.com>
2025-10-24 21:06:50 +0200

    Make some changes to the shell.

--- dev-user.nix
+++ dev-user.nix
@@ -4,17 +4,35 @@
   # Add Zsh as a user shell
   programs.zsh = {
     enable = true;
-    ohMyZsh.enable = true;
-    ohMyZsh.theme = "lambda";
+    enableCompletion = true;
     autosuggestions.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+
+    histSize = 2000;
+    histFile = "$HOME/.zsh_history";
+    setOptions = [
+      "HIST_IGNORE_ALL_DUPS"
+    ];
+
+    # Shell visuals
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "amuse";
+    };
   };
+
+  users.defaultUserShell = pkgs.zsh;
 
   # Create default user
   users.users.tom = {
     isNormalUser = true;
     description = "Tom S.";
     extraGroups = [ "networkmanager" "wheel" "dialout" ];
-    shell = pkgs.zsh;
     createHome = true;
   };
 }
