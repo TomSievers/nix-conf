83189a674390cb9e3378e63383967da5e9aad6f6
TomSievers <tom.sievers13@gmail.com>
2025-11-03 20:21:05 +0100

    It seems like the extension pack doesn't install other extensions, so install the necessary dev container extension.

--- home-manager.nix
+++ home-manager.nix
@@ -3,13 +3,11 @@
 with lib;
 
 let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+  home-manager = builtins.fetchTarball
+    "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
   cfg = config.user;
-in
-{
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
+in {
+  imports = [ (import "${home-manager}/nixos") ];
 
   options.user = {
     enable = mkEnableOption "Enable default software development user.";
@@ -62,131 +60,140 @@ in
     programs.zsh.enable = true;
 
     # Hook in the Home Manager config for this user
-    home-manager.users.${cfg.username} = { pkgs, ... }: mkMerge [
-      {
-        services.podman.enable = true;
-        programs.vim.enable = true;
+    home-manager.users.${cfg.username} = { pkgs, ... }:
+      mkMerge [
+        {
+          services.podman.enable = true;
+          programs.vim.enable = true;
 
-        # We also need to set this in home manager, otherwise we won't be able to install vscode.
-        nixpkgs.config.allowUnfree = true;
+          # We also need to set this in home manager, otherwise we won't be able to install vscode.
+          nixpkgs.config.allowUnfree = true;
 
-        # Hint for using wayland instead of X
-        home.sessionVariables.NIXOS_OZONE_WL = "1";
+          # Hint for using wayland instead of X
+          home.sessionVariables.NIXOS_OZONE_WL = "1";
 
-        programs.firefox = {
-          enable = true;
-          policies = {
-            BlockAboutConfig = false;
-            DefaultDownloadDirectory = "\${home}/Downloads";
-            ExtensionSettings = {
-              "uBlock0@raymondhill.net" = {
-                default_area = "menupanel";
-                install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-                installation_mode = "force_installed";
-                private_browsing = true;
+          programs.firefox = {
+            enable = true;
+            policies = {
+              BlockAboutConfig = false;
+              DefaultDownloadDirectory = "\${home}/Downloads";
+              ExtensionSettings = {
+                "uBlock0@raymondhill.net" = {
+                  default_area = "menupanel";
+                  install_url =
+                    "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+                  installation_mode = "force_installed";
+                  private_browsing = true;
+                };
               };
             };
           };
-        };
 
-        home.packages = with pkgs; [
-          # For docker/podman compose files.
-          podman-compose
+          home.packages = with pkgs; [
+            # For docker/podman compose files.
+            podman-compose
 
-          # GUI applications
-          rpi-imager
-          sourcegit
-          gparted
-          wineWowPackages.waylandFull
+            # GUI applications
+            rpi-imager
+            sourcegit
+            gparted
+            wineWowPackages.waylandFull
 
-          # Nerd font for zsh
-          nerd-fonts.adwaita-mono
+            # Nerd font for zsh
+            nerd-fonts.adwaita-mono
 
-        ];
+          ];
 
-        # Some sane default zsh configuration with some usefull aliases
-        programs.zsh = {
-          enable = true;
-          enableCompletion = true;
-          autosuggestion.enable = true;
-
-          shellAliases = {
-            ll = "ls -l";
-            update = "sudo nixos-rebuild switch";
-            system-upgrade = "sudo nixos-rebuild switch --upgrade";
-          };
-
-          history = {
-            size = 2000;
-            saveNoDups = true;
-          };
-
-          oh-my-zsh = {
+          # Some sane default zsh configuration with some usefull aliases
+          programs.zsh = {
             enable = true;
-            plugins = [ "git" ];
-            theme = cfg.zshTheme;
-          };
-        };
+            enableCompletion = true;
+            autosuggestion.enable = true;
 
-        # Configure git how I like it.
-        programs.git = {
-          enable = true;
-          package = pkgs.gitFull;
-          lfs.enable = true;
-          extraConfig = {
-            user = {
-              name = "Tom Sievers";
-              email = "t.sievers@hanskamp.com";
+            shellAliases = {
+              ll = "ls -l";
+              update = "sudo nixos-rebuild switch";
+              system-upgrade = "sudo nixos-rebuild switch --upgrade";
             };
 
-            init.defaultBranch = "master";
-
-            core.autocrlf = "input";
-          };
-        };
-
-        # Setup vscode with some basic extensions to allow most development to occur in dev containers.
-        programs.vscode = {
-          enable = true;
-          profiles.default = {
-            enableExtensionUpdateCheck = true;
-            enableUpdateCheck = true;
-            userSettings = {
-              "editor.formatOnSave" = true;
-              "terminal.integrated.defaultProfile.linux" = "zsh";
-              "git.confirmSync" = false;
+            history = {
+              size = 2000;
+              saveNoDups = true;
             };
-            extensions = with pkgs.vscode-extensions; [
-              ms-python.python
-              github.copilot
-              ms-vscode-remote.vscode-remote-extensionpack
-              jnoortheen.nix-ide
-            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              {
-                name = "docker";
-                publisher = "docker";
-                version = "0.18.0";
-                sha256 = "a5XlZL7jExYPhz1HODCgvw29JTf7DNyUBFQWYe+4dmA=";
-              }
-              {
-                name = "vscode-containers";
-                publisher = "ms-azuretools";
-                version = "2.2.0";
-                sha256 = "UxWsu7AU28plnT0QMdpPJrcYZIV09FeC+rmYKf39a6M=";
-              }
-              {
-                name = "git-graph";
-                publisher = "mhutchie";
-                version = "1.30.0";
-                sha256 = "sHeaMMr5hmQ0kAFZxxMiRk6f0mfjkg2XMnA4Gf+DHwA=";
-              }
-            ];
-          };
-        };
 
-        home.stateVersion = "25.05"; # or your system version
-      }
-      cfg.homeConfig
-    ];
+            oh-my-zsh = {
+              enable = true;
+              plugins = [ "git" ];
+              theme = cfg.zshTheme;
+            };
+          };
+
+          # Configure git how I like it.
+          programs.git = {
+            enable = true;
+            package = pkgs.gitFull;
+            lfs.enable = true;
+            extraConfig = {
+              user = {
+                name = "Tom Sievers";
+                email = "t.sievers@hanskamp.com";
+              };
+
+              init.defaultBranch = "master";
+
+              core.autocrlf = "input";
+            };
+          };
+
+          # Setup vscode with some basic extensions to allow most development to occur in dev containers.
+          programs.vscode = {
+            enable = true;
+            profiles.default = {
+              enableExtensionUpdateCheck = true;
+              enableUpdateCheck = true;
+              userSettings = {
+                "editor.formatOnSave" = true;
+                "terminal.integrated.defaultProfile.linux" = "zsh";
+                "git.confirmSync" = false;
+              };
+              extensions = with pkgs.vscode-extensions;
+                [
+                  ms-python.python
+                  github.copilot
+                  ms-vscode-remote.remote-ssh
+                  jnoortheen.nix-ide
+                ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                  {
+                    name = "docker";
+                    publisher = "docker";
+                    version = "0.18.0";
+                    sha256 = "a5XlZL7jExYPhz1HODCgvw29JTf7DNyUBFQWYe+4dmA=";
+                  }
+                  {
+                    name = "vscode-containers";
+                    publisher = "ms-azuretools";
+                    version = "2.2.0";
+                    sha256 = "UxWsu7AU28plnT0QMdpPJrcYZIV09FeC+rmYKf39a6M=";
+                  }
+                  {
+                    name = "git-graph";
+                    publisher = "mhutchie";
+                    version = "1.30.0";
+                    sha256 = "sHeaMMr5hmQ0kAFZxxMiRk6f0mfjkg2XMnA4Gf+DHwA=";
+                  }
+                  {
+                    name = "remote-containers";
+                    publisher = "ms-vscode-remote";
+                    version = "0.427.0";
+                    sha256 = "aSwC8NLJxelv2B+FnF8rmA5pnowugmdF+gnm+A0qSiE=";
+                  }
+                ];
+            };
+          };
+
+          home.stateVersion = "25.05"; # or your system version
+        }
+        cfg.homeConfig
+      ];
   };
 }
