c14051356eb577230494c125ddf2842d19ec1ebf
Tom Sievers <t.sievers@hanskamp.com>
2026-03-11 20:34:06 +0100

    Disable powersaving to prevent issue where wifi speed is crippled after suspend

--- network.nix
+++ network.nix
@@ -3,7 +3,9 @@
 {
   # Configure networkmanager.
   networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.powersave = true;
+  networking.networkmanager.wifi.powersave = false;
   # Make sure to use resolvconf
   networking.resolvconf.enable = true;
+
+  hardware.enableRedistributableFirmware = true;
 }
