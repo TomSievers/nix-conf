a9d8095dda001a9258010176863b27f4840731b5
TomSievers <tom.sievers13@gmail.com>
2026-06-06 13:30:54 +0200

    Add destkop config.

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,45 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../boot.nix
+    ../../audio.nix
+    ../../home-manager.nix
+    ../../gnome-de.nix
+    ../../locale.nix
+    ../../network.nix
+    ../../root-packages.nix
+    ../../desktop-fedora-sideload.nix
+    ../../hyprland.nix
+    ../../disks.nix
+    ../../power.nix
+    ../../generic.nix
+  ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  user = {
+    enable = true;
+    username = "tom";
+    description = "Tom S";
+    zshTheme = "agnoster";
+  };
+
+  # Enable optional mount at /data
+  disks.enableDataMount = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
