75074be15c6330d2ed5c92c64605127a30e8bdd6
Tom Sievers <t.sievers@hanskamp.com>
2026-09-25 11:22:07 +0200

    update: bamboo network pluign

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     open-bamboo-networking = {
-      url = "git+https://codeberg.org/TomSievers/open-bamboo-networking-nixos.git?ref=develop";
+      url = "git+https://codeberg.org/TomSievers/open-bamboo-networking-nixos.git?ref=master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
